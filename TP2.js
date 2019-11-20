da19ac2646a4ba09c59bb652cc1e17dc0918a325
Ciervos <55422681+Ciervos@users.noreply.github.com>
2019-11-20 12:18:00 -0300

    esqueleto

---
+++ TP2.js
@@ -0,0 +1,86 @@
+/*Comentarios para luego borrar:
+
+Ariana funciones 1,2 y 4
+Luján funciones 3, 5 y 6
+Rocio funciones 7,8 y 9
+
+Sobre el archivo test: la idea es que cada una tenga su propio archivo test para trabajar más comodo
+xq despues aunque tengamos los 
+tres archivos separados la consola los lee juntos.
+*/
+
+
+const vendedoras = ["Ada", "Grace", "Hedy", "Sheryl"];
+
+const ventas = [
+  [ 100000000, 4, 2, 2019, 'Grace', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500'] ],
+  [ 100000001, 1, 1, 2019, 'Ada', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500'] ],
+  [ 100000002, 2, 1, 2019, 'Grace', 'Caballito', ['Monitor ASC 543', 'Motherboard MZI', 'HDD Toyiva'] ],
+  [ 100000003, 10, 1, 2019, 'Ada', 'Centro', ['Monitor ASC 543', 'Motherboard ASUS 1200'] ],
+  [ 100000004, 12, 1, 2019, 'Grace', 'Caballito', ['Monitor GPRS 3000', 'Motherboard ASUS 1200'] ],
+  [ 100000005, 21, 3, 2019, 'Hedy', 'Caballito', ['Monitor ASC 543', 'Motherboard ASUS 1200', 'RAM Quinston'] ]
+]
+
+const precios = [
+  [ 'Monitor GPRS 3000', 200 ],
+  [ 'Motherboard ASUS 1500', 120 ],
+  [ 'Monitor ASC 543', 250 ],
+  [ 'Motherboard ASUS 1200', 100 ],
+  [ 'Motherboard MZI', 30 ],
+  [ 'HDD Toyiva', 90 ],
+  [ 'HDD Wezter Dishital', 75 ],
+  [ 'RAM Quinston', 110 ],
+  [ 'RAM Quinston Fury', 230 ]
+];
+
+const sucursales = ['Centro', 'Caballito'];
+
+
+/* --- funciones --- */
+const precioMaquina =(componentes) =>{
+};
+
+const cantidadVentasComponente =(componente) =>{
+};
+
+const ventasVendedora =(nombre) =>{
+};
+
+const componenteMasVendido =() =>{
+};
+
+const ventasSucursal =(sucursal) =>{
+};
+
+const mejorVendedora =() =>{
+};
+
+const ventaPromedio =() =>{
+};
+
+const obtenerIdVenta =() =>{
+};
+
+const agregarVenta =(dia, mes, anio, vendedora, sucursal, componentes) =>{
+};
+
+
+/* --- Fin de funciones --- */
+
+
+module.exports = {
+    vendedoras,
+    ventas,
+    precios,
+    surcursales,
+    precioMaquina,
+    cantidadVentasComponente,
+    ventasVendedora,
+    componenteMasVendido,
+    ventasSucursal,
+    mejorVendedora,
+    ventaPromedio,
+    obtenerIdVenta,
+    agregarVenta,
+
+};
