1ff6f902dea4556bbfe94883ae013e0689f67e60
Rocio del Pilar Esteban <hr.chio@gmail.com>
2019-11-30 23:08:29 -0300

    se agrego ejercicio 4 y se modifico detalles

--- TP2.js
+++ TP2.js
@@ -1,21 +1,14 @@
-/*Comentarios para luego borrar:
-
-Ariana funciones 1,2 y 4
-Luján funciones 3, 5 y 6
-Rocio funciones 7,8 y 9
-
-*/
-
 
 const vendedoras = ["Ada", "Grace", "Hedy", "Sheryl"];
 
-const ventas = [[ 100000000, 4, 2, 2019, 'Grace', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500'] ],
+const ventas = [
+  [ 100000000, 4, 2, 2019, 'Grace', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500'] ],
   [ 100000001, 1, 1, 2019, 'Ada', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500'] ],
   [ 100000002, 2, 1, 2019, 'Grace', 'Caballito', ['Monitor ASC 543', 'Motherboard MZI', 'HDD Toyiva'] ],
   [ 100000003, 10, 1, 2019, 'Ada', 'Centro', ['Monitor ASC 543', 'Motherboard ASUS 1200'] ],
   [ 100000004, 12, 1, 2019, 'Grace', 'Caballito', ['Monitor GPRS 3000', 'Motherboard ASUS 1200'] ],
   [ 100000005, 21, 3, 2019, 'Hedy', 'Caballito', ['Monitor ASC 543', 'Motherboard ASUS 1200', 'RAM Quinston'] ]
-]
+];
 
 const precios = [
   [ 'Monitor GPRS 3000', 200 ],
@@ -31,65 +24,122 @@ const precios = [
 
 const sucursales = ['Centro', 'Caballito'];
 
-
-/* --- funciones --- */
-//1.- hallar precio de maquina:
-
-//hallando el precio por componente:
+//1:
 const precioDeComponente = (componente) => {
   let componenteNombrePrecio = precios.find(elemento => elemento[0] === componente);
   return componenteNombrePrecio[1];
 }
-const precioMaquina =(componentes)=>{
-  return componentes.reduce((acumulador, componente)=>{
+const precioMaquina = (componentes) => {
+  return componentes.reduce((acumulador, componente) => {
     acumulador += precioDeComponente(componente);
     return acumulador;
-  },0)
+  }, 0)
 };
 
-//2.- 
-
-const cantidadVentasComponente =(componente) =>{
-  return ventas.reduce((acumulador, venta)=>{
+//2:
+const cantidadVentasComponente = (componente) => {
+  return ventas.reduce((acumulador, venta) => {
     acumulador += encontrarComponenteEnVenta(venta, componente);
     return acumulador;
   },0)
 };
 
-const encontrarComponenteEnVenta = (venta, componente) =>{
+const encontrarComponenteEnVenta = (venta, componente) => {
   const filtro = venta[6].filter(elemento => elemento == componente)
   return filtro.length
 }; 
 
-//3.- 
-const ventasVendedora = (nombre) =>{
-  return ventaPorVendedora(nombre).reduce((acumulador, venta)=>{
+//3:
+const ventasVendedora = (nombre) => {
+  let elementosVendidos = [];
+  let suma = 0;
+  for (let i = 0; i < ventas.length; i++){
+    if(nombre == ventas[i][4]){
+      for(let x = 0; x < ventas[i][4].length; x++){
+        elementosVendidos.push(ventas[i][6][x]);
+      }
+    }
+  }
+  for (let i = 0; i < elementosVendidos.length; i++){
+    for(let x = 0; x < precios.length; x++){
+      if(elementosVendidos[i] == precios[x][0]){
+          suma = suma + precios[x][1];  
+      }
+    }
+  }
+  return suma;
+};
+
+/* Otra posible solucion:
+const ventasVendedora = (nombre) => {
+  return ventaPorVendedora(nombre).reduce((acumulador, venta)=> {
     acumulador += precioMaquina(venta[6])
     return acumulador;
   }, 0);
 };
+const ventaPorVendedora = (nombre) => ventas.filter(venta => venta[4] == nombre);
+*/
 
-const ventaPorVendedora = (nombre) =>{
-  const encontrarVenta = ventas.filter(venta =>venta[4]== nombre)
-  return encontrarVenta;
-} 
-
-// 4.- 
-const componenteMasVendido =() =>{
+// 4:
+const componenteMasVendido = () => {
+  let componenteMasVendido = "";
+  let cantidadMaxima = 0;
+  for (componente of precios){
+    let cantidad = cantidadVentasComponente(componente[0]);
+    if(cantidad > cantidadMaxima){
+      componenteMasVendido = componente[0];
+      cantidadMaxima = cantidad;
+    }
+  }
+  return componenteMasVendido;
 };
 
-const ventasSucursal =(sucursal) =>{
+// 5:
+const ventasSucursal = (sucursal) => {
+  let elementosVendidos = [];
+  let suma = 0;
+  for (let i = 0; i < ventas.length; i++){
+    if(sucursal == ventas[i][5]){
+      for(let x = 0; x < ventas[i][5].length; x++){
+        elementosVendidos.push(ventas[i][6][x]);
+      }
+    }
+  }
+  for (let i = 0; i < elementosVendidos.length; i++){
+    for(let x = 0; x < precios.length; x++){
+      if(elementosVendidos[i] == precios[x][0]){
+        suma = suma + precios[x][1];  
+      }
+    }
+  }
+ return suma;
+};
+/*Otra posible solución:
+const ventasSucursal = (sucursal) => {
+  return ventaPorSucursal(sucursal).reduce((acumulador, venta)=> {
+    acumulador += precioMaquina(venta[6])
+    return acumulador;
+  }, 0);
+};
+const ventaPorSucursal = (sucursal) => ventas.filter(venta => venta[5] == sucursal);
+*/
+
+//6:
+const mejorVendedora = () => {
+  let laMejor = "";
+  for(let i = 0; i < vendedoras.length; i++){
+    if (ventasVendedora(laMejor) < ventasVendedora(vendedoras[i])){
+      laMejor = vendedoras[i];
+    } 
+  }
+  return laMejor;
 };
 
-const mejorVendedora =() =>{
-};
-
-// 7.-Hallando venta promedio de la venta total:
-const ventaPromedio = () =>{
-  let promedio = ventaSumaTotal()/ totalProductoVendido();
+// 7:
+const ventaPromedio = () => {
+  let promedio = ventaSumaTotal() / totalProductoVendido();
   return Math.floor(promedio);
 };
-
 //halando sumatotal:
 const ventaSumaTotal = () => {
   return ventas.reduce((acumulador, venta) => {
@@ -98,26 +148,25 @@ const ventaSumaTotal = () => {
     return acumulador;
   }, 0);
 }
-
 //hallando el total de productos vendidos:
-const totalProductoVendido = ()=>{
-  return ventas.reduce((acumulador,venta) =>{
+const totalProductoVendido = () => {
+  return ventas.reduce((acumulador, venta) => {
     let cantComponentes = venta[6].length;
     acumulador += cantComponentes;
     return acumulador;
   }, 0);
 }
 
-//8.- Obtener el Id de venta entre 100000000 y 999999999:
-// idAleatorio = Math.floor(Math.random()*(Y-X))+X
-
-const obtenerIdVenta =() =>{
+//8:
+//Se usa la siguiente formulacion: idAleatorio = Math.floor(Math.random()*(Y-X))+X
+const obtenerIdVenta = () => {
   const IdAleatorio = Math.floor(Math.random() * (999999999 - 100000000) + 100000000);
   return IdAleatorio;
 };
 
 //9.- agregarVenta(id,dia, mes, anio, vendedora, sucursal, componentes)
-const agregarVenta =(dia, mes, anio, vendedora, sucursal, componentes) =>{
+const agregarVenta = (dia, mes, anio, vendedora, sucursal, componentes) =>{
+  ventas.push([obtenerIdVenta(), dia, mes, anio, vendedora, sucursal, componentes]);
 };
 
 
@@ -125,18 +174,21 @@ const agregarVenta =(dia, mes, anio, vendedora, sucursal, componentes) =>{
 
 
 module.exports = {
-    vendedoras,
-    ventas,
-    precios,
-    sucursales,
-    precioMaquina,
-    cantidadVentasComponente,
-    ventasVendedora,
-    componenteMasVendido,
-    ventasSucursal,
-    mejorVendedora,
-    ventaPromedio,
-    obtenerIdVenta,
-    agregarVenta,
-
+  precioDeComponente,
+  vendedoras,
+  ventas,
+  precios,
+  sucursales,
+  precioMaquina,
+  encontrarComponenteEnVenta,
+  cantidadVentasComponente,
+  ventasVendedora,
+  componenteMasVendido,
+  ventasSucursal,
+  mejorVendedora,
+  ventaPromedio,
+  totalProductoVendido,
+  ventaSumaTotal, 
+  obtenerIdVenta,
+  agregarVenta,
 };
--- TP2.test.js
+++ TP2.test.js
@@ -1,69 +1,68 @@
 const trabajo = require('./tp2'),
+precioDeComponente = trabajo.precioDeComponente;
+encontrarComponenteEnVenta = trabajo.encontrarComponenteEnVenta;
 vendedoras = trabajo.vendedoras;
 ventas = trabajo.ventas;
 precios = trabajo.precios;
 sucursales = trabajo.surcursales;
 precioMaquina = trabajo.precioMaquina;
-cantidadVentasComponentes = trabajo.cantidadVentasComponentes;
+cantidadVentasComponente = trabajo.cantidadVentasComponente;
 ventasVendedora = trabajo.ventasVendedora;
 componenteMasVendido= trabajo.componenteMasVendido;
 ventasSucursal = trabajo.ventasSucursal;
 mejorVendedora = trabajo.mejorVendedora;
 ventaPromedio = trabajo.ventaPromedio;
+totalProductoVendido = trabajo.totalProductoVendido;
+ventaSumaTotal = trabajo.ventaSumaTotal;
 obtenerIdVenta = trabajo.obtenerIdVenta;
 agregarVenta = trabajo.agregarVenta;
 
+ 
 
-
-
-
-
-
-test ('precio de cada componente', ()=>{
+test ('Función 1: precio de cada componente', () => {
   expect (precioDeComponente('Monitor GPRS 3000')).toBe(200)
 });
-test('precio de cada maquina', ()=>{
-  expect (precioMaquina(['Monitor GPRS 3000', 'Motherboard ASUS 1500'])).toBe(320)
+test('precio de cada maquina', () => {
+  expect(precioMaquina(['Monitor GPRS 3000', 'Motherboard ASUS 1500'])).toBe(320)
 });
-test('encontrar componente por venta', ()=>{
-  expect (encontrarComponenteEnVenta(
-    [100000000, 4, 2, 2019, 'Grace', 'Centro', 
-    ['Monitor GPRS 3000', 'Motherboard ASUS 1500'],
-    'Motherboard ASUS 1500'],'Motherboard ASUS 1500')).toBe(1)
+test ('Función 2:cantidad de venta por componente', () => {
+    expect(cantidadVentasComponente('Motherboard ASUS 1500')).toBe(2)
+  });
+test('encontrar componente por venta', () => {
+  expect(encontrarComponenteEnVenta(
+      [100000000, 4, 2, 2019, 'Grace', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500'], 'Motherboard ASUS 1500'],
+      'Motherboard ASUS 1500'))
+    .toBe(1)
 });
-test ('cantidad de venta por componente', () =>{
-  expect (cantidadVentasComponente('Motherboard ASUS 1500')).toBe(2)
+test('Funcion 3: retorna el importe total de ventas realizadas por vendedora', () => {
+    expect(ventasVendedora("Grace")).toBe(990);
+    expect(ventasVendedora("Ada")).toBe(670); 
 });
-test ('encontrar componente en venta', () =>{
-  expect ( encontrarComponenteEnVenta(
-    [ 100000000, 4, 2, 2019, 'Grace', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500'] ],
-    'Motherboard ASUS 1500').toBe(1))
+test('Función 4: componente mas vendido', () => {
+    expect(componenteMasVendido()).toBe('Monitor GPRS 3000')
+})
+test('Función 5: retorna el importe de las ventas totales realizadas por una sucursal', () => {  
+    expect(ventasSucursal("Centro")).toBe(990);
 });
-
-test ('venta total por vendedora', () =>{
-  expect (ventasVendedora('Ada')).toBe(670) 
+test('Función 6: Devuelve el nombre de la vendedora que más ingresos generó', () => {
+    expect(mejorVendedora() ).toBe("Grace");
+}); 
+test('Función 7: promedio de venta total', () => {
+    expect(ventaPromedio()).toBe(151)   
 });
-test ('venta por cada vendedora', ()=>{
-  expect (ventaPorVendedora('Ada')).toHaveLength(2)
+test('suma total de productos vendidos', () => {
+  expect(totalProductoVendido()).toBe(14)
 });
-test ('suma total de productos vendidos', ()=>{
-  expect (totalProductoVendido()).toBe(14)
+test('venta suma total', () => {
+  expect(ventaSumaTotal()).toBe(2120)
 });
-test ('venta suma total', () =>{
-  expect (ventaSumaTotal()).toBe(2114)
-});
-test ('promedio de venta total', ()=>{
-  expect (ventaPromedio()).toBe(151)   
-});
-test ('obteniendo un id de venta aleatorio entre 100000000 y 999999999', ()=>{
-
+test('Función 8: Obtener un id de venta aleatorio entre 100000000 y 999999999', () => {
   let idVenta = obtenerIdVenta ();
   let valorAleatorio = (idVenta > 100000000 && idVenta  < 999999999);
-
-  expect (valorAleatorio).toBe(true)   
+  expect(valorAleatorio).toBeTruthy(); 
 });
-test ('agregando venta nueva', ()=>{
+test('Función 9: agregando venta nueva', () => {
   const cantActual= ventas.length;
-  agregarVenta(26, 12, 2019, 'harriet', 'belgrano', ['RAM Quinston Fury','HDD Wezter Dishital']);
+  agregarVenta(26, 12, 2019, 'harriet', 'belgrano', ['RAM Quinston Fury', 'HDD Wezter Dishital']);
   expect(ventas).toHaveLength(cantActual + 1);
 });
